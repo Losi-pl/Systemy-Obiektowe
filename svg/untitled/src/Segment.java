1f317e7290478226d1d2149868602c2a6268b9a2
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-02-25 17:01:32 +0100

    Zadanie 5

--- svg/untitled/src/Segment.java
+++ svg/untitled/src/Segment.java
@@ -3,4 +3,17 @@ public class Segment {
 
     public float length()
     { return (float)Math.sqrt(Math.pow(Math.abs(p1.x - p2.x), 2) + Math.pow(Math.abs(p1.y - p2.y), 2)); }
+
+    public static Segment findLongest(Segment[] coll)
+    {
+        int lon_ind = 0;
+        float len = coll[0].length();
+        for(int i = 0; i < coll.length; i++)
+            if(coll[i].length() > len)
+            {
+                lon_ind = i;
+                len = coll[i].length();
+            }
+        return coll[lon_ind];
+    }
 }
