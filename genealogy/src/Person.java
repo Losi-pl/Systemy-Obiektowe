03cbc514d7fdefd3134a4fa32429eaf9bedc90b9
Bogdan <bogdan.derdz@gmail.com>
2026-04-15 15:59:06 +0200

    15.04.2026 -> Kontenery -> Zadanie 2

--- genealogy/src/Person.java
+++ genealogy/src/Person.java
@@ -1,7 +1,28 @@
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Person
 {
-    String FirstName, LastName;
+    String firstName, lastName;
     LocalDate birthDate;
+    Set<Person> children;
+
+    public Person(String firstName, String lastName, LocalDate birthDate)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        children = new LinkedHashSet<Person>();
+    }
+
+    public Person(String FirstName, String LastName)
+    {
+        this(FirstName, LastName, LocalDate.now());
+    }
+
+    public boolean adopt(Person child_or_prisoner)
+    {
+        return children.add(child_or_prisoner);
+    }
 }
